1757c9328b95a726063cfb166fb9ad4ef4206492
Ray Agni <jagni001@ucr.edu>
2021-08-09 16:23:13 -0700

    FEAT: Completed Front End

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-// import './App.css';
 import pokedex from "./Pokedex";
 import Pokecard from "./Pokecard";
 
 function App() {
   return (
-    <div>
-      <ul className="App">
-        {pokedex.map(p => <Pokecard 
+    <div className="container">
+      <div className="App row">
+        { pokedex.map(p => <Pokecard 
                           id={p.id} 
                           name={p.name} 
                           type={p.type} 
                           base_experience={p.base_experience}/>)}
-      </ul>
+      </div>
     </div>
   );
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -3,14 +3,15 @@ import "./Pokecard.css";
 const BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
 function Pokecard(props) {
+
   return (
-   <div className="Pokecard">
+   <div className="Pokecard col-3">
     <h3 className="Pokecard-name">{props.name}</h3>
-    <img src={`${BASE_URL}${props.id}.png`} className="Pokecard" alt={props.name}/>
+    <img src={`${BASE_URL}${props.id}.png`} className="Pokecard-img" alt={props.name}/>
     <p>Type: {props.type}</p>
     <p>EXP: {props.base_experience}</p>
    </div>
- ); 
+ );  
 }
 
 export default Pokecard;
